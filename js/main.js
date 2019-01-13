70ccc1a93674b791d577af6a1c13d9f08696a2db
Rowan <rowantatai@gmail.com>
2019-01-13 01:59:02 -0500

    initial draft

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$('nav.mobile').click(function(){
+	$(this).toggleClass('open');
+	$('.mobileNav_content').toggleClass('open');
+});
+
+$('.mobile_nav_item').click(function(){
+	$('nav.mobile').toggleClass('open');
+	$('.mobileNav_content').toggleClass('open');
+});
+
+$('#p0').click(function(){
+	window.open("#");
+});
+
+$('#p1').click(function(){
+	window.open("#");
+});
+
+$('#p2').click(function(){
+	window.open("#");
+});
+
+$('#p3').click(function(){
+	window.open("#");
+});
+
+$('#p4').click(function(){
+	window.open("#");
+});
